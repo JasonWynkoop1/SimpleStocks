40c2665ffa46dca76a97297ae6f6df419df789c6
Jason <jasonwynkoop1@yahoo.com>
2019-10-29 18:53:39 -0400

    Added contact activity...having some issues with send button (Null pointer)

--- app/src/main/java/com/cs360/simplestocks/activities/ContactUsActivity.java
+++ app/src/main/java/com/cs360/simplestocks/activities/ContactUsActivity.java
@@ -1,14 +1,111 @@
 package com.cs360.simplestocks.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.androidtutorialshub.loginregister.R;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.widget.NestedScrollView;
 
-public class ContactUsActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.androidtutorialshub.loginregister.R;
+import com.cs360.simplestocks.helpers.InputValidation;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+
+import java.util.Objects;
+
+public class ContactUsActivity extends AppCompatActivity implements View.OnClickListener {
+    private final AppCompatActivity activity = ContactUsActivity.this;
+    private InputValidation inputValidation;
+    private NestedScrollView nestedScrollView;
+
+    private AppCompatButton appCompatSendButton;
+
+    private TextInputLayout mTextInputLayoutEmail;
+    private TextInputLayout mtextInputLayoutName;
+
+    private EditText mEditText;
+    private TextInputEditText mTextInputEditTextEmail;
+    private TextInputEditText mTextInputEditName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        initializeViews();
+        initializeListeners();
+        initializeObjects();
+    }
+
+    //TODO:Figure out why this isn't working
+    /**
+     * initializing listeners
+     */
+    private void initializeListeners(){
+        //appCompatSendButton.setOnClickListener(this);
+    }
+
+    /**
+     * Implemented method to listen for the click on view
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.appCompatButtonLogin:
+                verifyInput();
+                Intent it = new Intent(Intent.ACTION_SEND);
+                it.putExtra(Intent.EXTRA_EMAIL, new String[]{mTextInputEditTextEmail.getText().toString()});
+                it.putExtra(Intent.EXTRA_TEXT,mEditText.getText());
+                it.setType("message/rfc822");
+                startActivity(Intent.createChooser(it,"Choose Mail App"));
+                break;
+        }
+    }
+
+
+    /**
+     * initializing view
+     */
+    private void initializeViews(){
+        nestedScrollView = findViewById(R.id.NESTED_SCROLL_VIEW);
+        mTextInputEditName = findViewById(R.id.TEXT_INPUT_EDIT_NAME);
+        mtextInputLayoutName = findViewById(R.id.TEXT_INPUT_LAYOUT_NAME);
+        mTextInputLayoutEmail = findViewById(R.id.TEXT_INPUT_LAYOUT_EMAIL);
+        mTextInputEditTextEmail = findViewById(R.id.textInputEditTextEmail);
+        appCompatSendButton = findViewById(R.id.BUTTON_SEND_MESSAGE);
+        mEditText = findViewById(R.id.TEXT_INPUT_LAYOUT_MESSAGE_CONTACT_US);
+    }
+
+    /**
+     * initializing objects
+     *
+     * mSQLiteDatabaseHelper
+     * input validator
+     */
+    private void initializeObjects(){
+        inputValidation = new InputValidation(activity);
+    }
+
+    /**
+     *
+     */
+    private void verifyInput(){
+        if (!inputValidation.checkForUserInput(mTextInputEditName, mtextInputLayoutName, getString(R.string.error_message_name))) {
+            return;
+        }
+        if (!inputValidation.checkForUserInput(mTextInputEditTextEmail, mTextInputLayoutEmail, getString(R.string.error_message_email))){
+            return;
+        }
+        if (!inputValidation.checkForValidEmail(mTextInputEditTextEmail, mTextInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+
+
     }
 }
