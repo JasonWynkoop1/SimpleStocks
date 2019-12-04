8c76959173fe1cbcef241b95e7266f696ab32ba9
Jason Wynkoop <jasonwynkoop1@yahoo.com>
2019-12-03 19:03:31 -0500

    Working sign-out

--- app/src/main/java/com/cs360/simplestocks/activities/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/cs360/simplestocks/activities/ui/settings/SettingsFragment.java
@@ -1,11 +1,13 @@
 package com.cs360.simplestocks.activities.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
+import com.cs360.simplestocks.activities.LoginActivities.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.simplestocks.loginregister.R;
 
@@ -13,24 +15,29 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import static com.simplestocks.loginregister.R.id.sign_out_button;
+
 
 public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SettingsViewModel settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView signOutButton = root.findViewById(R.id.sign_out_button);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        Button signOutButton = view.findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(this);
         settingsViewModel.getText().observe(this, s -> {
 
         });
-        return root;
+        return view;
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.sign_out_button:
+            case sign_out_button:
                 FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
         }
     }
 
